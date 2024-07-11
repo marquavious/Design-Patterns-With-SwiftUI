9fff5be89e877e08b8ea338654f240d41ac7d4f0
marquavious <marq.draggon@gmail.com>
2024-07-11 01:25:56 -0400

    Added Strategy pattern

--- Design Patterns.playground/Pages/Strategy.xcplaygroundpage/Contents.swift
+++ Design Patterns.playground/Pages/Strategy.xcplaygroundpage/Contents.swift
@@ -1,8 +1,8 @@
 //: [Previous](@previous)
 
 import Foundation
-
-let arrayOfNumbers = [1,6,4,5,3,9,43,2,7,23,10]
+import SwiftUI
+import PlaygroundSupport
 
 protocol SortStrategy {
     func sort(_ data: [Int]) -> [Int]
@@ -38,12 +38,106 @@ class Sorter {
     }
 }
 
-let forwardSortSorter = Sorter(sortStrategy: ForwardSort())
-let backwardSortSorter = Sorter(sortStrategy: BackwardSort())
-let randomSortSorter = Sorter(sortStrategy: RandomSort())
+enum SortingStrategy: Int, CaseIterable, Equatable, Hashable {
+    case forwardSort = 0
+    case backwardSort
+    case randomSort
 
-print(forwardSortSorter.sortData(arrayOfNumbers)) // [1, 2, 3, 4, 5, 6, 7, 9, 10, 23, 43]
-print(backwardSortSorter.sortData(arrayOfNumbers)) // [10, 23, 7, 2, 43, 9, 3, 5, 4, 6, 1]
-print(randomSortSorter.sortData(arrayOfNumbers)) // [43, 23, 6, 9, 1, 2, 10, 7, 4, 3, 5] (Random)
+    var title: String {
+        switch self {
+        case .forwardSort:
+            "Forward Sort"
+        case .backwardSort:
+            "Backward Sort"
+        case .randomSort:
+            "Random Sort"
+        }
+    }
+}
 
+struct ContentView: View {
+
+    @State var numbers = [Int]()
+    @State var newNumberText = String()
+    @FocusState private var textFieldInFocus: Bool
+    @State private var selectedSortingStrategy: SortingStrategy = .forwardSort
+
+    let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+
+    var body: some View {
+        IPhoneView {
+            VStack {
+                ScrollView(.vertical) {
+                    LazyVGrid(columns: columns) {
+                        ForEach(numbers, id: \.self) { number in
+                            Text(String(number))
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 50)
+                                .background(.red, in: RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        }
+                    }.padding(10)
+                }
+
+                Spacer()
+
+                VStack(spacing: 16) {
+                    Text("Pick a Sorting Algorithim")
+                    Picker(String(), selection: $selectedSortingStrategy) {
+                        Text(SortingStrategy.forwardSort.title).tag(SortingStrategy.forwardSort)
+                        Text(SortingStrategy.backwardSort.title).tag(SortingStrategy.backwardSort)
+                        Text(SortingStrategy.randomSort.title).tag(SortingStrategy.randomSort)
+                    }
+                    .pickerStyle(.segmented)
+                    .onChange(of: selectedSortingStrategy) {
+                        sortNumbers()
+                    }
+
+                    TextField("Add Number", text: $newNumberText)
+                        .onSubmit {
+                            guard
+                                let newNumber = Int(newNumberText),
+                                !numbers.contains(newNumber) else { return }
+                            addNumer(int: newNumber)
+                            sortNumbers()
+                        }
+                        .focused($textFieldInFocus)
+                        .keyboardType(.numberPad)
+
+                    Color.gray
+                        .opacity(0.5)
+                        .frame(minWidth: 1, maxHeight: 1)
+                    Button("Add Number") {
+                        guard
+                            let newNumber = Int(newNumberText),
+                            !numbers.contains(newNumber) else { return }
+                        addNumer(int: newNumber)
+                        sortNumbers()
+                    }
+                    .disabled(newNumberText.isEmpty)
+                }
+                .padding(20)
+            }
+        }
+    }
+
+    func addNumer(int: Int) {
+        numbers.append(int)
+        sortNumbers()
+        newNumberText = String()
+        textFieldInFocus = true
+    }
+
+    func sortNumbers() {
+        switch selectedSortingStrategy {
+        case .forwardSort:
+            numbers = Sorter(sortStrategy: ForwardSort()).sortData(numbers)
+        case .backwardSort:
+            numbers = Sorter(sortStrategy: BackwardSort()).sortData(numbers)
+        case .randomSort:
+            numbers = Sorter(sortStrategy: RandomSort()).sortData(numbers)
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
 //: [Next](@next)
--- Design Patterns.playground/Sources/SwiftUIHelpers.swift
+++ Design Patterns.playground/Sources/SwiftUIHelpers.swift
@@ -4,22 +4,27 @@ import SwiftUI
 // MARK: - IPHONE VIEW
 
 public struct IPhoneView<Content: View>: View {
+
     let content: Content
     let multiplier: CGFloat
+    let phoneWidth: CGFloat
+    let phoneHeight: CGFloat
 
-    public init(multiplier: CGFloat = 2, @ViewBuilder content: () -> Content) {
+    public init(multiplier: CGFloat = 2, phoneWidth: CGFloat = 250, phoneHeight: CGFloat = 500, @ViewBuilder content: () -> Content) {
         self.multiplier = multiplier
         self.content = content()
+        self.phoneWidth = phoneWidth
+        self.phoneHeight = phoneHeight
     }
 
     public var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 50)
-                .frame(width: 250 * multiplier, height: 500 * multiplier)
+                .frame(width: phoneWidth * multiplier, height: phoneHeight * multiplier)
                 .background(Color(red: 64 / 255, green: 64 / 255, blue: 70 / 255))
             RoundedRectangle(cornerRadius: 15)
                 .fill(Color.white)
-                .frame(width: 220 * multiplier, height: 450 * multiplier)
+                .frame(width: (phoneWidth - 30) * multiplier, height: (phoneHeight - 50) * multiplier)
                 .overlay(alignment: .center) {
                     content
                 }
